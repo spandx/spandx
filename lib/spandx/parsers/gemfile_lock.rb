52183869e25b0b3f524d6c7c3c520c820e8d56e5
mo khan <mo.khan@gmail.com>
2019-12-27 22:04:46 -0700

    extract method to parse dependencies

--- lib/spandx/parsers/gemfile_lock.rb
+++ lib/spandx/parsers/gemfile_lock.rb
@@ -9,12 +9,29 @@ module Spandx
 
       def parse(lockfile)
         report = { version: '1.0', packages: [] }
-        parser = ::Bundler::LockfileParser.new(IO.read(lockfile))
-        parser.dependencies.each do |key, dependency|
-          spec = dependency.to_spec
-          report[:packages].push(name: key, version: spec.version.to_s, spdx: spec.license)
+        dependencies_from(lockfile) do |dependency|
+          report[:packages].push(map_from(dependency))
         end
         report
+      end
+
+      private
+
+      def dependencies_from(lockfile)
+        ::Bundler::LockfileParser
+          .new(IO.read(lockfile))
+          .dependencies.each do |_key, dependency|
+          yield dependency
+        end
+      end
+
+      def map_from(dependency)
+        spec = dependency.to_spec
+        {
+          name: dependency.name,
+          version: spec.version.to_s,
+          spdx: spec.license
+        }
       end
     end
   end
