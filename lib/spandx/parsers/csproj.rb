10777dd4f7328bef59bb851550bed1bb58dce540
mo khan <mo.khan@gmail.com>
2020-01-24 17:24:23 -0700

    Move recursion in to project file

--- lib/spandx/parsers/csproj.rb
+++ lib/spandx/parsers/csproj.rb
@@ -6,9 +6,7 @@ module Spandx
       class ProjectFile
         attr_reader :catalogue, :document, :nuget
 
-        def initialize(path, nuget, catalogue)
-          @catalogue = catalogue
-          @nuget = nuget
+        def initialize(path)
           @path = path
           @dir = File.dirname(path)
           @document = Nokogiri::XML(IO.read(path))
@@ -18,27 +16,12 @@ module Spandx
           document.search('//ProjectReference').map do |node|
             relative_project_path = node.attribute('Include').value.strip.tr('\\', '/')
             absolute_project_path = File.expand_path(File.join(@dir, relative_project_path))
-            self.class.new(absolute_project_path, nuget, catalogue)
+            self.class.new(absolute_project_path)
           end
         end
 
         def package_references
-          document.search('//PackageReference').map do |node|
-            name = attribute_for('Include', node)
-            version = attribute_for('Version', node)
-            Dependency.new(
-              name: name,
-              version: version,
-              licenses: nuget.licenses_for(name, version).map { |x| catalogue[x] }
-            )
-          end
-        end
-
-        private
-
-        def attribute_for(key, node)
-          node.attribute(key)&.value&.strip ||
-            node.at_xpath("./#{key}")&.content&.strip
+          project_references.map(&:package_references).flatten + document.search('//PackageReference')
         end
       end
 
@@ -47,15 +30,25 @@ module Spandx
       end
 
       def parse(lockfile)
-        project_file = ProjectFile.new(lockfile, nuget, catalogue)
-        (
-          project_file.project_references.map(&:package_references) +
-          project_file.package_references
-        ).flatten.compact
+        project_file = ProjectFile.new(lockfile)
+        project_file.package_references.map do |node|
+          name = attribute_for('Include', node)
+          version = attribute_for('Version', node)
+          Dependency.new(
+            name: name,
+            version: version,
+            licenses: nuget.licenses_for(name, version).map { |x| catalogue[x] }
+          )
+        end
       end
 
       def nuget
         @nuget ||= Gateways::Nuget.new
+      end
+
+      def attribute_for(key, node)
+        node.attribute(key)&.value&.strip ||
+          node.at_xpath("./#{key}")&.content&.strip
       end
     end
   end
