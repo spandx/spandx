60b21c723b6714c35e71be13615c63d7d9b2ff82
mo khan <mo.khan@gmail.com>
2020-01-24 17:02:06 -0700

    scan csproj files that depend on other projects

--- lib/spandx/parsers/csproj.rb
+++ lib/spandx/parsers/csproj.rb
@@ -3,32 +3,55 @@
 module Spandx
   module Parsers
     class Csproj < Base
+      class ProjectFile
+        def initialize(path, nuget, catalogue)
+          @catalogue = catalogue
+          @nuget = nuget
+          @path = path
+          @dir = File.dirname(path)
+          @document = Nokogiri::XML(IO.read(path))
+        end
+
+        def dependencies
+          project_references(document) + package_references(document)
+        end
+
+        private
+
+        attr_reader :catalogue, :document, :nuget
+
+        def project_references(document)
+          document.search('//ProjectReference').map do |node|
+            relative_project_path = node.attribute('Include').value.strip.tr('\\', '/')
+            absolute_project_path = File.expand_path(File.join(@dir, relative_project_path))
+            self.class.new(absolute_project_path, nuget, catalogue).dependencies
+          end.flatten
+        end
+
+        def package_references(document)
+          document.search('//PackageReference').map do |node|
+            name = attribute_for('Include', node)
+            version = attribute_for('Version', node)
+            Dependency.new(
+              name: name,
+              version: version,
+              licenses: nuget.licenses_for(name, version).map { |x| catalogue[x] }
+            )
+          end
+        end
+
+        def attribute_for(key, node)
+          node.attribute(key)&.value&.strip ||
+            node.at_xpath("./#{key}")&.content&.strip
+        end
+      end
+
       def self.matches?(filename)
         filename.match?(/.*\.csproj/)
       end
 
       def parse(lockfile)
-        document = from_xml(IO.read(lockfile))
-        document.search('//PackageReference').map do |node|
-          name = attribute_for('Include', node)
-          version = attribute_for('Version', node)
-          Dependency.new(
-            name: name,
-            version: version,
-            licenses: nuget.licenses_for(name, version).map { |x| catalogue[x] }
-          )
-        end
-      end
-
-      private
-
-      def from_xml(xml)
-        Nokogiri::XML(xml)
-      end
-
-      def attribute_for(key, node)
-        node.attribute(key)&.value&.strip ||
-          node.at_xpath("./#{key}")&.content&.strip
+        ProjectFile.new(lockfile, nuget, catalogue).dependencies
       end
 
       def nuget
--- spec/unit/parsers/csproj_spec.rb
+++ spec/unit/parsers/csproj_spec.rb
@@ -20,5 +20,17 @@ RSpec.describe Spandx::Parsers::Csproj do
       specify { expect(jive.version).to eql('0.1.0') }
       specify { expect(jive.licenses.map(&:id)).to match_array(['MIT']) }
     end
+
+    context 'when parsing a .csproj file that has a reference to another project' do
+      let(:lockfile) { fixture_file('nuget/nested/test.csproj') }
+
+      let(:because) do
+        VCR.use_cassette(File.basename(lockfile)) do
+          subject.parse(lockfile)
+        end
+      end
+
+      specify { expect(because.map(&:name)).to match_array(%w[jive xunit]) }
+    end
   end
 end
