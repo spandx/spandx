d08a1f14ad18e9f5554805c04363defc6f798c3a
mo khan <mo.khan@gmail.com>
2020-01-27 11:24:21 -0700

    Perform similarity analysis on all files in spdx catalogue

--- spec/unit/guess_spec.rb
+++ spec/unit/guess_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Spandx::Guess do
   let(:catalogue) { Spandx::Catalogue.from_file(fixture_file('spdx.json')) }
 
   describe '#license_for' do
-    it 'guesses the license correctly' do
-      catalogue.each do |license|
-        expect(subject.license_for(license_file(license.id))).to eql(license.id)
+    Dir["spec/fixtures/spdx/jsonld/*.jsonld"].map { |x| File.basename(x).gsub('.jsonld', '') }.each do |license|
+      it "guesses the `#{license}` correctly" do
+        expect(subject.license_for(license_file(license))).to eql(license)
       end
     end
   end
