02dc9d8c5cd31624ef67226cff148e8cf50c94cd
mo khan <mo.khan@gmail.com>
2020-01-28 10:13:38 -0700

    Parse sln files

--- lib/spandx/parsers/sln.rb
+++ lib/spandx/parsers/sln.rb
@@ -1,10 +1,27 @@
 module Spandx
   module Parsers
-    class Sln
+    class Sln < Base
       def parse(file_path)
-        IO.read(file_path)
-        []
+        paths = IO.readlines(file_path).map do |line|
+          next unless line.match?(/^\s*Project\(/)
+
+          path = line.split('"')[5]
+          next unless path
+
+          path = path.tr("\\", "/")
+          next unless path.match?(/\.[a-z]{2}proj$/)
+
+          dir = File.dirname(file_path)
+          path = File.join(dir, path)
+          Pathname.new(path).cleanpath.to_path
+        end.compact
+        paths.map do |path|
+          parser = Parsers.for(path, catalogue: catalogue)
+          parser.parse(path)
+        end.flatten
       end
+
+      private
     end
   end
 end
--- spec/unit/parsers/sln_spec.rb
+++ spec/unit/parsers/sln_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe Spandx::Parsers::Sln do
+  subject { described_class.new(catalogue: catalogue) }
+  let(:catalogue) { Spandx::Catalogue.from_file(fixture_file('spdx.json')) }
+
   describe "#parse" do
     context "when parsing a sln file without any project references" do
       let(:sln) { fixture_file('nuget/empty.sln') }
@@ -6,6 +9,18 @@ RSpec.describe Spandx::Parsers::Sln do
       it 'returns an empty list of dependencies' do
         expect(subject.parse(sln)).to be_empty
       end
+    end
+
+    context "when parsing a sln file with a single project reference" do
+      let(:sln) { fixture_file('nuget/single.sln') }
+
+      let(:because) do
+        VCR.use_cassette(File.basename(sln)) do
+          subject.parse(sln)
+        end
+      end
+
+      specify { expect(because.map(&:name)).to match_array(%w[jive xunit]) }
     end
   end
 end
