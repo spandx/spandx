4be43b6def8a1d01e87c6ce4d159ff5c1313565e
mo khan <mo.khan@gmail.com>
2020-01-30 15:01:46 -0700

    Fix linter errors

--- lib/spandx/index.rb
+++ lib/spandx/index.rb
@@ -12,7 +12,7 @@ module Spandx
     def update!
       json = fetch('https://api.nuget.org/v3/catalog0/index.json')
       json['items'].take(1).each do |item|
-        json = fetch(item["@id"])
+        json = fetch(item['@id'])
         json['items'].take(1).each do |other_item|
           build_index_for(fetch(other_item['@id']))
         end
@@ -27,7 +27,7 @@ module Spandx
     end
 
     def build_index_for(package)
-      sha1 = Digest::SHA1.hexdigest(File.join("api.nuget.org", package['id'], package['version']))
+      sha1 = Digest::SHA1.hexdigest(File.join('api.nuget.org', package['id'], package['version']))
       path = File.join(directory, *sha1.scan(/../))
       FileUtils.mkdir_p(path)
     end
--- spec/unit/index_spec.rb
+++ spec/unit/index_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe Spandx::Index do
 
   describe '#update!' do
     it 'creates an index for nuget packages' do
-      VCR.use_cassette("nuget-catalogue") do
+      VCR.use_cassette('nuget-catalogue') do
         subject.update!
 
-        sha1 = Digest::SHA1.hexdigest("api.nuget.org/Lykke.Service.Operations.Client/2.2.8")
+        sha1 = Digest::SHA1.hexdigest('api.nuget.org/Lykke.Service.Operations.Client/2.2.8')
         expected_path = File.join(directory, sha1.scan(/../).join('/'))
         expect(Dir).to exist(expected_path)
       end
