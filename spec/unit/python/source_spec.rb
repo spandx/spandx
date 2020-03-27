dcfbafe2777deb1f39248c843eb4ae31b51f5d8f
mo khan <mo.khan@gmail.com>
2020-03-26 20:43:30 -0600

    Fix linter errors

--- spec/unit/python/source_spec.rb
+++ spec/unit/python/source_spec.rb
@@ -4,31 +4,30 @@ RSpec.describe Spandx::Python::Source do
   context 'when fetching metadata for a known package from https://pypi.org' do
     subject { described_class.default }
 
-    it 'fetches the correct data' do
+    let(:result) do
       VCR.use_cassette('pypi/pytest-5.4.1') do
-        result = subject.lookup('pytest', '5.4.1')
-
-        expect(result).not_to be_nil
-        expect(result['info']).not_to be_nil
-        expect(result['info']['name']).to eql('pytest')
-        expect(result['info']['version']).to eql('5.4.1')
+        subject.lookup('pytest', '5.4.1')
       end
     end
+
+    specify { expect(result).not_to be_nil }
+    specify { expect(result['info']).not_to be_nil }
+    specify { expect(result['info']['name']).to eql('pytest') }
+    specify { expect(result['info']['version']).to eql('5.4.1') }
   end
 
   context 'when fetching metadata for a known package from https://test.pypi.org' do
     subject { described_class.new({ 'name' => 'pypi', 'url' => 'https://test.pypi.org/simple', 'verify_ssl' => true }) }
 
-    it 'fetches the correct data' do
+    let(:result) do
       VCR.use_cassette('test.pypi/pip-18.1') do
-        result = subject.lookup('pip', '18.1')
-
-        puts result.inspect
-        expect(result).not_to be_nil
-        expect(result['info']).not_to be_nil
-        expect(result['info']['name']).to eql('pip')
-        expect(result['info']['version']).to eql('18.1')
+        subject.lookup('pip', '18.1')
       end
     end
+
+    specify { expect(result).not_to be_nil }
+    specify { expect(result['info']).not_to be_nil }
+    specify { expect(result['info']['name']).to eql('pip') }
+    specify { expect(result['info']['version']).to eql('18.1') }
   end
 end
