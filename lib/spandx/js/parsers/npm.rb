7d1e2e6cc740fe0294a7170fedfb7e60745ead99
mo khan <mo.khan@gmail.com>
2020-04-06 16:55:33 -0600

    Fix linter errors

--- lib/spandx/js/parsers/npm.rb
+++ lib/spandx/js/parsers/npm.rb
@@ -10,29 +10,35 @@ module Spandx
 
         def parse(file_path)
           items = Set.new
-          file_content = File.open(file_path).read
-          package_lock = JSON.parse(file_content)
-          package_lock['dependencies'].each do |dependency, metadata|
-            uri = URI.parse(metadata['resolved'])
-            source = "#{uri.scheme}://#{uri.host}"
-            items.add(
-              Spandx::Core::Dependency.new(
-                name: dependency,
-                version: metadata['version'],
-                licenses: gateway.licenses_for(dependency, metadata['version'], source: source),
-                meta: metadata
-              )
-            )
+          each_metadata(file_path) do |metadata|
+            items.add(map_from(metadata))
           end
-
           items
         end
 
         private
 
+        def each_metadata(file_path)
+          package_lock = JSON.parse(IO.read(file_path))
+          package_lock['dependencies'].each do |name, metadata|
+            yield metadata.merge('name' => name)
+          end
+        end
+
         def gateway
           # yarn and npm using same api end points
           @gateway ||= YarnPkg.new(catalogue: catalogue)
+        end
+
+        def map_from(metadata)
+          uri = URI.parse(metadata['resolved'])
+          source = "#{uri.scheme}://#{uri.host}"
+          Spandx::Core::Dependency.new(
+            name: metadata['name'],
+            version: metadata['version'],
+            licenses: gateway.licenses_for(metadata['name'], metadata['version'], source: source),
+            meta: metadata
+          )
         end
       end
     end
