5eec2dea834319a9554c6231e152b045c125e5ff
mo khan <mo.khan@gmail.com>
2020-04-06 20:06:27 -0600

    Fix linter error

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,6 +6,6 @@ VCR.configure do |x|
   x.cassette_library_dir = 'spec/fixtures/recordings'
   x.hook_into :webmock
   x.default_cassette_options = {
-    match_requests_on: [:method, :host, :path]
+    match_requests_on: %i[method host path]
   }
 end
