dda0efe1dcd1b42f55e57e17d06b24f7fc5a0ebb
mo khan <mo.khan@gmail.com>
2020-04-12 15:57:38 -0600

    Inline variables

--- lib/spandx/dotnet/license_plugin.rb
+++ lib/spandx/dotnet/license_plugin.rb
@@ -3,25 +3,22 @@
 module Spandx
   module Dotnet
     class LicensePlugin < Spandx::Core::Plugin
-      def initialize(catalogue: Spdx::Catalogue.from_git)
+      def initialize(catalogue: Spdx::Catalogue.from_git, package_manager: :nuget)
         @guess = Core::Guess.new(catalogue)
-        @cache = ::Spandx::Core::Cache.new(:nuget)
-        @api = ::Spandx::Dotnet::NugetGateway.new
-        @gateway = ::Spandx::Core::CompositeGateway.new(@cache, @api)
+        @gateway = ::Spandx::Core::CompositeGateway.new(
+          ::Spandx::Core::Cache.new(package_manager),
+          ::Spandx::Dotnet::NugetGateway.new
+        )
       end
 
       def enhance(dependency)
         return dependency unless dependency.managed_by?(:nuget)
 
-        gateway.licenses_for(dependency.name, dependency.version).map do |text|
+        @gateway.licenses_for(dependency.name, dependency.version).map do |text|
           dependency.licenses << @guess.license_for(text)
         end
         dependency
       end
-
-      private
-
-      attr_reader :gateway
     end
   end
 end
