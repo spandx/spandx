d76bb7f28ffa1bbaeb0b2906729d31eacbc5a9f9
mo khan <mo.khan@gmail.com>
2020-04-12 17:02:27 -0600

    Collapse dotnet license plugin into generic one

--- lib/spandx/core/license_plugin.rb
+++ lib/spandx/core/license_plugin.rb
@@ -1,23 +1,33 @@
 # frozen_string_literal: true
 
 module Spandx
-  module Dotnet
+  module Core
     class LicensePlugin < Spandx::Core::Plugin
       def initialize(catalogue: Spdx::Catalogue.from_git)
         @guess = Core::Guess.new(catalogue)
       end
 
       def enhance(dependency)
-        return dependency unless dependency.managed_by?(:nuget)
+        return dependency unless known?(dependency.package_manager)
 
         gateway = ::Spandx::Core::CompositeGateway.new(
           ::Spandx::Core::Cache.for(dependency.package_manager),
-          ::Spandx::Dotnet::NugetGateway.new
+          gateway_for(dependency)
         )
         gateway.licenses_for(dependency.name, dependency.version).each do |text|
           dependency.licenses << @guess.license_for(text)
         end
         dependency
+      end
+
+      private
+
+      def known?(package_manager)
+        [:nuget].include?(package_manager)
+      end
+
+      def gateway_for(dependency)
+        ::Spandx::Dotnet::NugetGateway.new
       end
     end
   end
--- spec/unit/core/license_plugin_spec.rb
+++ spec/unit/core/license_plugin_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe Spandx::Dotnet::LicensePlugin do
+RSpec.describe Spandx::Core::LicensePlugin do
   subject { described_class.new }
+  let(:catalogue) { ::Spandx::Spdx::Catalogue.from_file(fixture_file('spdx/json/licenses.json')) }
 
   describe '#enhance' do
-    context 'when the dependency is not managed by the `nuget` package manager' do
-      let(:dependency) { ::Spandx::Core::Dependency.new(package_manager: :rubygems, name: 'spandx', version: '0.1.0') }
+    context 'when the dependency is not managed by a known package manager' do
+      let(:dependency) { ::Spandx::Core::Dependency.new(package_manager: :unknown, name: 'spandx', version: '0.1.0') }
 
       specify { expect(subject.enhance(dependency)).to eql(dependency) }
     end
