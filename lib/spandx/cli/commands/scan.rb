bda97276f175dfa19e281c2d72411d21a8b0bc79
mo khan <mo.khan@gmail.com>
2020-04-30 17:25:50 -0600

    extract path traversal

--- lib/spandx/cli/commands/scan.rb
+++ lib/spandx/cli/commands/scan.rb
@@ -14,31 +14,38 @@ module Spandx
 
         def execute(output: $stdout)
           report = ::Spandx::Core::Report.new
-          each_file_in(scan_path) do |file|
+
+          Spandx::Core::PathTraversal.new(scan_path, recursive: @options['recursive']).each do |file|
             each_dependency_from(file) do |dependency|
               report.add(dependency)
             end
           end
+
+          #each_file_in(scan_path) do |file|
+            #each_dependency_from(file) do |dependency|
+              #report.add(dependency)
+            #end
+          #end
           output.puts(format(report.to(@options[:format])))
         end
 
         private
 
-        def recursive?
-          @options['recursive']
-        end
+        #def recursive?
+          #@options['recursive']
+        #end
 
-        def each_file_in(dir, &block)
-          files = File.directory?(dir) ? Dir.glob(File.join(dir, '*')) : [dir]
-          files.each do |file|
-            if File.directory?(file)
-              each_file_in(file, &block) if recursive?
-            else
-              Spandx.logger.debug(file)
-              block.call(file)
-            end
-          end
-        end
+        #def each_file_in(dir, &block)
+          #files = File.directory?(dir) ? Dir.glob(File.join(dir, '*')) : [dir]
+          #files.each do |file|
+            #if File.directory?(file)
+              #each_file_in(file, &block) if recursive?
+            #else
+              #Spandx.logger.debug(file)
+              #block.call(file)
+            #end
+          #end
+        #end
 
         def each_dependency_from(file)
           ::Spandx::Core::Parser
