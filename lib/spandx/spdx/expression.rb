c96fe784c16cdd5690c5a64c1b0fe7b79ea9434f
mo khan <mo.khan@gmail.com>
2020-05-06 21:58:15 -0600

    fix linter errors

--- lib/spandx/spdx/expression.rb
+++ lib/spandx/spdx/expression.rb
@@ -1,54 +1,57 @@
+# frozen_string_literal: true
 
 module Spandx
   module Spdx
     class Expression < Parslet::Parser
-=begin
-https://spdx.org/spdx-specification-21-web-version
-
-idstring              = 1*(ALPHA / DIGIT / "-" / "." )
-license-id            = <short form license identifier in Appendix I.1>
-license-exception-id  = <short form license exception identifier in Appendix I.2>
-license-ref           = ["DocumentRef-"1*(idstring)":"]"LicenseRef-"1*(idstring)
-simple-expression = license-id / license-id”+” / license-ref
-compound-expression =  1*1(simple-expression /
-                            simple-expression "WITH" license-exception-id /
-                            compound-expression "AND" compound-expression /
-                            compound-expression "OR" compound-expression ) /
-                          "(" compound-expression ")" )
-
-license-expression =  1*1(simple-expression / compound-expression)
-=end
+      # https://spdx.org/spdx-specification-21-web-version
+      #
+      # idstring              = 1*(ALPHA / DIGIT / "-" / "." )
+      # license-id            = <short form license identifier in Appendix I.1>
+      # license-exception-id  = <short form license exception identifier in Appendix I.2>
+      # license-ref           = ["DocumentRef-"1*(idstring)":"]"LicenseRef-"1*(idstring)
+      # simple-expression = license-id / license-id"+" / license-ref
+      # compound-expression =  1*1(simple-expression /
+      #                             simple-expression "WITH" license-exception-id /
+      #                             compound-expression "AND" compound-expression /
+      #                             compound-expression "OR" compound-expression ) /
+      #                           "(" compound-expression ")" )
+      #
+      # license-expression =  1*1(simple-expression / compound-expression)
       rule(:lparen) { str('(') }
       rule(:rparen) { str(')') }
       rule(:digit) { match('\d') }
       rule(:space) { match('\s') }
       rule(:alpha) { match['a-zA-Z'] }
+      rule(:colon) { str(':') }
       rule(:dot) { str('.') }
+      rule(:plus) { str('+') }
       rule(:hyphen) { str('-') }
       rule(:with_op) { str('with') }
-      rule(:and_op) { str('AND') | str("and") }
+      rule(:and_op) { str('AND') | str('and') }
       rule(:or_op) { str('OR') | str('or') }
 
-      #idstring              = 1*(ALPHA / DIGIT / "-" / "." )
+      # idstring              = 1*(ALPHA / DIGIT / "-" / "." )
       rule(:id_character) { alpha | digit | hyphen | dot }
-      rule(:id_string) { id_character.repeat(1, 15) }
+      rule(:id_string) { id_character.repeat(1) }
 
       # license-id = <short form license identifier in Appendix I.1>
       rule(:license_id) do
         id_string
       end
 
+      # license-ref = ["DocumentRef-"1*(idstring)":"]"LicenseRef-"1*(idstring)
       rule(:license_ref) do
+        str('DocumentRef-') >> id_string >> colon >> str('LicenseRef-') >> id_string
       end
 
-      # simple-expression = license-id / license-id”+” / license-ref
+      # simple-expression = license-id / license-id"+" / license-ref
       rule(:simple_expression) do
-        license_id# | license_ref
+        license_id >> plus.maybe | license_ref
       end
 
       # license-exception-id = <short form license exception identifier in Appendix I.2>
       rule(:license_exception_id) do
-        # TODO:: Update to match exceptions list
+        # TODO: : Update to match exceptions list
         str('389-exception')
       end
 
@@ -59,26 +62,26 @@ license-expression =  1*1(simple-expression / compound-expression)
 
       # compound-expression "AND" compound-expression
       rule(:and_expression) do
-        #compound_expression >> space >> and_op >> space >> compound_expression
+        # compound_expression >> space >> and_op >> space >> compound_expression
         simple_expression >> space >> and_op >> space >> simple_expression
       end
 
       # compound-expression "OR" compound-expression
       rule(:or_expression) do
-        #compound_expression >> space >> or_op >> space >> compound_expression
+        # compound_expression >> space >> or_op >> space >> compound_expression
         simple_expression >> space >> or_op >> space >> simple_expression
       end
 
-      #compound-expression =  1*1(
-      #  simple-expression /
-      #  simple-expression "WITH" license-exception-id /
-      #  compound-expression "AND" compound-expression /
-      #  compound-expression "OR" compound-expression ) /
-      #  "(" compound-expression ")"
+      #  compound-expression =  1*1(
+      #    simple-expression /
+      #    simple-expression "WITH" license-exception-id /
+      #    compound-expression "AND" compound-expression /
+      #    compound-expression "OR" compound-expression ) /
+      #    "(" compound-expression ")"
       #  )
       rule(:compound_expression) do
         lparen.maybe >> (
-          #simple_expression.repeat(1, 1) |
+          # simple_expression.repeat(1, 1) |
           with_expression.repeat(1, 1) |
           and_expression.repeat(1, 1) |
           or_expression.repeat(1, 1)
--- spec/unit/spdx/expression_spec.rb
+++ spec/unit/spdx/expression_spec.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 RSpec.describe Spandx::Spdx::Expression do
   subject { described_class.new }
 
-  describe "#parse" do
+  describe '#parse' do
     specify { expect(subject).to parse('MIT') }
-    specify { expect(subject.parse('MIT').to_s).to eql("MIT") }
+    specify { expect(subject.parse('MIT').to_s).to eql('MIT') }
 
     specify { expect(subject.parse_with_debug('MIT or GPLv3')).to be_truthy }
     specify { expect(subject.parse_with_debug('(0BSD OR MIT)')).to be_truthy }
@@ -18,38 +20,38 @@ RSpec.describe Spandx::Spdx::Expression do
     specify { expect(subject.parse_with_debug('BSD-3-Clause OR MIT')).to be_truthy }
   end
 
-  describe "#simple_expression" do
+  describe '#simple_expression' do
     specify { expect(subject.simple_expression).to parse('MIT') }
     specify { expect(subject.simple_expression).to parse('0BSD') }
     specify { expect(subject.simple_expression).to parse('BSD-3-Clause') }
     specify { expect(subject.simple_expression).to parse('GPLv3') }
   end
 
-  describe "#license_id" do
+  describe '#license_id' do
     specify { expect(subject.license_id).to parse('MIT') }
     specify { expect(subject.license_id).to parse('0BSD') }
   end
 
-  describe "$license_exception_id" do
+  describe '$license_exception_id' do
     specify { expect(subject.license_exception_id).to parse('389-exception') }
   end
 
-  describe "#with_expression" do
+  describe '#with_expression' do
   end
 
-  describe "#and_expression" do
+  describe '#and_expression' do
     subject { described_class.new.and_expression }
 
     specify { expect(subject.parse_with_debug('0BSD AND MIT')).to be_truthy }
   end
 
-  describe "#or_expression" do
+  describe '#or_expression' do
     subject { described_class.new.or_expression }
 
     specify { expect(subject.parse_with_debug('0BSD OR MIT')).to be_truthy }
   end
 
-  describe "#compound_expression" do
+  describe '#compound_expression' do
     subject { described_class.new.compound_expression }
 
     specify { expect(subject.parse_with_debug('0BSD AND MIT')).to be_truthy }
