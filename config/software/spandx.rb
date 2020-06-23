bc340d86804022139d393673e10d4dba2ac685ec
mo khan <mo.khan@gmail.com>
2020-06-22 23:04:56 -0600

    Specify default_version

--- config/software/spandx.rb
+++ config/software/spandx.rb
@@ -1,8 +1,7 @@
-#require_relative '../../lib/spandx/version'
+require_relative '../../lib/spandx/version'
 
 name "spandx"
-#version Spandx::VERSION
-version "0.13.5"
+default_version Spandx::VERSION
 
 license :MIT
 skip_transitive_dependency_licensing true
