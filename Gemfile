656bc2011d4b27277dd5898033dfd913c0481dae
mo khan <mo.khan@gmail.com>
2020-06-22 22:00:01 -0600

    Start to build omnibus package

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in spandx.gemspec
 gemspec
+gem 'omnibus-software', github: 'chef/omnibus-software'
---
+++ config/projects/spandx.rb
@@ -0,0 +1,25 @@
+#
+# Copyright 2020 YOUR NAME
+#
+# All Rights Reserved.
+#
+
+name "spandx"
+maintainer "mo.khan@gmail.com"
+homepage "https://github.com/spandx/spandx"
+
+# Defaults to C:/spandx on Windows
+# and /opt/spandx on all other platforms
+install_dir "#{default_root}/#{name}"
+
+build_version Omnibus::BuildVersion.semver
+build_iteration 1
+
+# Creates required build directories
+dependency "preparation"
+
+# spandx dependencies/components
+# dependency "somedep"
+
+exclude "**/.git"
+exclude "**/bundler/git"
