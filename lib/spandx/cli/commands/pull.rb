9a0f1bc1c0637bc07fbfc72f7edc4f7c0d23fee2
mo khan <mo.khan@gmail.com>
2020-11-18 15:59:32 -0700

    feat: display spinner while pulling the latest cache

--- lib/spandx/cli/commands/pull.rb
+++ lib/spandx/cli/commands/pull.rb
@@ -10,16 +10,30 @@ module Spandx
 
         def execute(output: $stdout)
           Spandx.git.each_value do |db|
-            output.puts "Updating #{db.url}..."
-            db.update!
+            with_spinner("Updating #{db.url}...", output: output) do
+              db.update!
+            end
           end
-          output.puts "Rebuilding index..."
-          Spandx::Core::Dependency::PACKAGE_MANAGERS.values.uniq.each do |type|
-            Spandx::Core::Cache
-              .new(type, root: Spandx.git[:cache].root.join('.index'))
-              .rebuild_index
+
+          with_spinner('Rebuilding index...', output: output) do
+            Spandx::Core::Dependency::PACKAGE_MANAGERS.values.uniq.each do |type|
+              Spandx::Core::Cache
+                .new(type, root: Spandx.git[:cache].root.join('.index'))
+                .rebuild_index
+            end
           end
           output.puts 'OK'
+        end
+
+        private
+
+        def with_spinner(message, output:)
+          spinner = TTY::Spinner.new("[:spinner] #{message}", clear: false, output: output)
+          spinner.auto_spin
+          yield
+          spinner.success('(done)')
+        ensure
+          spinner.stop
         end
       end
     end
