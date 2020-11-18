db277c4acdff071b94f2626e1cfcc67f1b4de6a3
mo khan <mo.khan@gmail.com>
2020-11-18 16:04:34 -0700

    refactor: extract methods to sync and build

--- lib/spandx/cli/commands/pull.rb
+++ lib/spandx/cli/commands/pull.rb
@@ -8,24 +8,33 @@ module Spandx
           @options = options
         end
 
-        def execute(output: $stdout)
+        def execute(output: $stderr)
+          update_git_dbs(output)
+          rebuild_index(output, ::Spandx::Core::Dependency::PACKAGE_MANAGERS.values.uniq)
+          output.puts 'OK'
+        end
+
+        private
+
+        def update_git_dbs(output)
           Spandx.git.each_value do |db|
             with_spinner("Updating #{db.url}...", output: output) do
               db.update!
             end
           end
+        end
+
+        def rebuild_index(output, sources)
+          index_path = Spandx.git[:cache].root.join('.index')
 
           with_spinner('Rebuilding index...', output: output) do
-            Spandx::Core::Dependency::PACKAGE_MANAGERS.values.uniq.each do |type|
+            sources.each do |source|
               Spandx::Core::Cache
-                .new(type, root: Spandx.git[:cache].root.join('.index'))
+                .new(source, root: index_path)
                 .rebuild_index
             end
           end
-          output.puts 'OK'
         end
-
-        private
 
         def with_spinner(message, output:)
           spinner = TTY::Spinner.new("[:spinner] #{message}", clear: false, output: output)
